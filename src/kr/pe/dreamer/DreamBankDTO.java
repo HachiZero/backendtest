ba87b29d15b0a45533d62d039c798430b92299b7
hachizero <helpme@i-um.net>
2016-10-23 02:43:08 -0700

    DreamBankDTO 개발

---
+++ src/kr/pe/dreamer/DreamBankDTO.java
@@ -0,0 +1,67 @@
+package kr.pe.dreamer;
+
+public class DreamBankDTO {
+	private String acount_num;
+	private String acount_pw;
+	private String name;
+	private String reg_num_f;
+	private String reg_num_b;
+	private String home;
+	private String id;
+	private String pw;
+	
+	public DreamBankDTO() {
+		
+	}
+	
+	public String getAcount_num() {
+		return acount_num;
+	}
+	public void setAcount_num(String acount_num) {
+		this.acount_num = acount_num;
+	}
+	public String getAcount_pw() {
+		return acount_pw;
+	}
+	public void setAcount_pw(String acount_pw) {
+		this.acount_pw = acount_pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getReg_num_f() {
+		return reg_num_f;
+	}
+	public void setReg_num_f(String reg_num_f) {
+		this.reg_num_f = reg_num_f;
+	}
+	public String getReg_num_b() {
+		return reg_num_b;
+	}
+	public void setReg_num_b(String reg_num_b) {
+		this.reg_num_b = reg_num_b;
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	
+}
